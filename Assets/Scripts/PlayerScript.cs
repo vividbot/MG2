7b0e123cefc4262da78d36536ecfa2d1d798a179
Sean <rseanelston@gmail.com>
2026-09-11 20:35:23 -0700

    Final working build

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,23 +1,37 @@
 using UnityEngine;
 using UnityEditor;
+using TMPro;
 public class PlayerScript : MonoBehaviour
 {
 
     public Rigidbody2D rb;
     [SerializeField] private float _jumpForce = 10f;
+    private bool _isGrounded;
+    [SerializeField] private TMP_Text _score;
+    private float _currentScore;
 
 
-    // [SerializeField] private
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void _CollectCoin()
     {
-        // rb = GetComponent<Rigidbody2D>();
+        _currentScore += 1;
+        _score.text = "Score - " + _currentScore;
+    }
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created 
+    void Start()
+    {   _currentScore = 0;
+        _score.text = "Score - " + _currentScore;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("KeyCode.Space"))
+
+
+
+        if (Input.GetButtonDown("Jump") && _isGrounded == true)
         {
             rb.AddForce(Vector2.up * _jumpForce,ForceMode2D.Impulse);  
         }
@@ -25,6 +39,17 @@ public class PlayerScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D trigger)
     {
-        
+        _CollectCoin();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isGrounded = true;
+        Debug.Log("Grounded");
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        _isGrounded = false;
+        Debug.Log("Not Grounded");
     }
 }
